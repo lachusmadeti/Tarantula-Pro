93d4b1e55b3a21b3945650f1b3874d3d82b08354
Javier Fernández <cuevadellobo@gmail.com>
2020-08-15 02:43:29 +0200

    Update Configuration.h

--- MKS SGEN L v1.0/Marlin 2.0.6/Power Los Recovery/Configuration.h
+++ MKS SGEN L v1.0/Marlin 2.0.6/Power Los Recovery/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Nuno´s 3D printer"
+#define CUSTOM_MACHINE_NAME "3D printer"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
