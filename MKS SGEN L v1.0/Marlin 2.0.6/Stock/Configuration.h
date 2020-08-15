05ba130d838b58a11b863a3b6b866172e1ac6c70
Javier Fernández <cuevadellobo@gmail.com>
2020-08-15 18:10:14 +0200

    Update Configuration.h

--- MKS SGEN L v1.0/Marlin 2.0.6/Stock/Configuration.h
+++ MKS SGEN L v1.0/Marlin 2.0.6/Stock/Configuration.h
@@ -1500,9 +1500,9 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 215
+#define PREHEAT_1_TEMP_HOTEND 190
 #define PREHEAT_1_TEMP_BED     60
-#define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 230
