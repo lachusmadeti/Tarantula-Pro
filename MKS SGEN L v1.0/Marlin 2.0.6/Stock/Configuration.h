b6f5ba497974b01c0b9701cb5a6657a6ee5f24fa
Javier Fernández <cuevadellobo@gmail.com>
2020-08-15 17:43:16 +0200

    Update Configuration.h

--- MKS SGEN L v1.0/Marlin 2.0.6/Stock/Configuration.h
+++ MKS SGEN L v1.0/Marlin 2.0.6/Stock/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Arañita"
+#define CUSTOM_MACHINE_NAME "Impresora 3D"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
